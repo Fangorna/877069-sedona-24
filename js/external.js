de26f4a42f596d2380f7f5129b003a1dec13c2a9
Fangorna <fangorna@yahoo.com>
2019-03-25 03:40:16 +0300

    вторая

--- js/external.js
+++ js/external.js
@@ -1,7 +1,16 @@
 var link = document.querySelector(".search");
-        var popup = document.querySelector(".popup-search");
-            popup.classList.remove("popup-no-js");
-        link.addEventListener("click", function (evt) {
-            evt.preventDefault();
-            popup.classList.toggle("popup-show");
-        });
+var popup = document.querySelector(".popup-search");
+var form = document.querySelector(".popup-form");
+var adate = document.querySelector("[name=arrival-date]");
+var ddate = document.querySelector("[name=departure-date]");
+popup.classList.remove("popup-no-js");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup-show");
+});
+form.addEventListener("submit", function (evt) {
+    if (!adate.value || !ddate.value) {
+       evt.preventDefault();
+       popup.classList.add("popup-error");
+    }
+});
